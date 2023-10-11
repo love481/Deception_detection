13ade9743a72d55bef2f3d68ced805411d5a3ba1
love481 <075bei016.love@pcampus.edu.np>
2023-10-11 09:48:44 +0545

    Random_forest_classifier approach added

---
+++ Random_forest_classifier/test_refined.py
@@ -0,0 +1,56 @@
+# Python Test code for testing Lie Detection
+from lie2_refined import extract_frames_and_predict,action_unit_lie_predict_from_n_frames,args
+import time,os,cv2
+
+# load n images from the folder and store in matrix 
+def load_from_folder(folder_dir):
+    list_images=[] 
+    index=1
+    for images in os.listdir(folder_dir):
+        # load only first n images in folder
+        if ((images.endswith(".jpg") or images.endswith(".png")) and index<=args.len_cut):
+            list_images.append(cv2.imread(os.path.join(folder_dir,images)))
+            index+=1
+    return list_images
+
+
+# Test routine for testing n images multiple of 30
+# Here,argument len_cut has value equal to 60 in lie2_refined.py file, so n=60 frames
+# Set folder containing n images and load it for processing
+folder_dir = "./folder_dir/"
+# test_input_images=load_from_folder(folder_dir)
+
+
+# Predict ML output 
+# singleoutput= action_unit_lie_predict_from_n_frames(test_input_images)
+# print(singleoutput)
+
+
+# Get the current time in seconds
+start_time = time.time()
+# Test routine for testing a whole video from file
+timestamped_outputs=extract_frames_and_predict(folder_dir + "t76.mp4")
+# Note to ignore exception
+# list of predictions for each defined_time_segments
+end_time = time.time()  # Get the current time again
+execution_time = end_time - start_time
+# Execution time is 28 seconds for above video
+print(f"Execution time: {execution_time:.6f} seconds")
+print(timestamped_outputs)
+
+# Sample of outputs
+# 0 to 2 sec of video [([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0], 0),
+# 2 to 4 sec of video ([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0], 0),
+# 4 to 6 sec of video ([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0], 0),
+# ([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0], 0),
+# ([0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 0),
+# ([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0], 0),
+# ([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0], 0),
+# ([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1], 0),
+# ([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1], 0),
+# ([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], 1),
+# ([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], 0), 
+# ([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1], 0)]
+
+
+
